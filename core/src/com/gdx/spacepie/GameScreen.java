493fda766f40f07b88e56bb8562411c2b844389e
poonsak.nuch@gmail.com <poonsak.n@ku.th>
2016-10-29 18:04:58 +0700

    add rocketImg

--- core/src/com/gdx/spacepie/GameScreen.java
+++ core/src/com/gdx/spacepie/GameScreen.java
@@ -1,16 +1,23 @@
 package com.gdx.spacepie;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen extends ScreenAdapter {
 	private SpacePie spacePie;
+	private Texture rocketImg;
 	
 	public GameScreen(SpacePie spacePie) {
 		this.spacePie = spacePie;
+		rocketImg = new Texture("rocket.png");
 	}
 
 	@Override
 	public void render(float delta) {
-		System.out.println("Hello " + delta);
+		SpriteBatch batch = spacePie.batch;
+		batch.begin();
+		batch.draw(rocketImg, 100, 100);
+		batch.end();
 	}
 }
