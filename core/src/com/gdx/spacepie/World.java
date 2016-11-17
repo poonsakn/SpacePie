934ecdfe5429218f94ace2d861bb9b64a97f3107
Poonsak <poonsak.nuch@gmail.com>
2016-11-17 22:14:53 +0700

    sp v1.01
    added sound

--- core/src/com/gdx/spacepie/World.java
+++ core/src/com/gdx/spacepie/World.java
@@ -4,18 +4,25 @@ import java.util.List;
 
 import javax.security.auth.callback.ChoiceCallback;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
 public class World {
 	private static Rocket rocket;
 	int a = 0;
 	int score = 0;
 	int highScore = 0;
 	boolean justLaunchGame = true;
+	int exploded = 0;
 	static List <Asteroid> asteroids = new ArrayList <Asteroid> ();
 	static List <Point> points = new ArrayList <Point> ();
-	
+	private Sound sound = Gdx.audio.newSound(Gdx.files.internal("Window - The Album Leaf.mp3"));
+	private Sound explosionSound  = Gdx.audio.newSound(Gdx.files.internal("explosion2.mp3"));
 	World (SpacePie spacePie) {
 		init(spacePie);
+		sound.loop();
 	}
+	
 	public void init(SpacePie spacePie) {
 //		if (a != 0) {
 //			justLaunchGame = false;
@@ -32,6 +39,7 @@ public class World {
 			points.add(new Point());
 		}
 		rocket = new Rocket(0,0,asteroids, points);
+		exploded = 0;
 	}
 	
 	public static Rocket getRocket() {
@@ -54,6 +62,11 @@ public class World {
 		}
 		if (a == 0) {
 			rocket.updatePosition();
+		} else if ((a != 0) && (exploded == 0)) {
+			exploded = 1;
+		} if (exploded == 1) {
+			explosionSound.play();
+			exploded++;
 		}
 	}
 	
